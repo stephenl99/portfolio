f790af15012b540d7009eb5d225224f0d1e1b71d
slinder8 <sjtlinder@icloud.com>
2024-11-08 16:36:30 -0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from './text-1731099881792.png';
+import { useState, useEffect } from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+          <header className="App-header">
+              <div className={"navbar"}>
+                  <div>
+                      <img className={"logo"} src={logo} alt="logo"/>
+                  </div>
+                  <ul className="navbarList">
+                      <li className="navbarElement" href="#home">Home</li>
+                      <li className="navbarElement" href="#news">About</li>
+                      <li className="navbarElement" href="#contact">Projects</li>
+                      <li className="navbarElement" href="#about">Resumé</li>
+                      <li className="navbarElement" href="#about">Contact</li>
+                  </ul>
+              </div>
+          </header>
+              <p>
+                  Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                  Learn React
+              </a>
+      </div>
   );
 }
 
